4ba5cc68850df0c482ecfdb770cf225754d10e98
Andrey Chebunin <andrey.tchebunin@yandex.ru>
2021-03-19 01:00:28 +0800

    Task 3

--- main.py
+++ main.py
@@ -41,5 +41,37 @@ class Cookbook:
         return self.list_dishes
 
 
+class Merging:
+    def merging_files(self, *args):
+        # подсчет строк в каждом файле
+        count_line_file = {}
+        for file in args:
+            line_count = 0
+            with open(file) as f:
+                for _ in f:
+                    line_count += 1
+                    count_line_file[file] = line_count
+
+        # сортировка файлов по количеству строк
+        # line = []
+        # for key, value in count_line_file.items():
+        #     line.append(value)
+        #     line.sort()
+        list_d = list(count_line_file.items())
+        list_d.sort(key=lambda i: i[1])
+
+        for sort_file in list_d:
+            with open(sort_file[0]) as num_f:
+                data = num_f.read()
+            with open('sorted/merge.txt', 'a') as merge_f:
+                merge_f.write(sort_file[0])
+                merge_f.write('\n')
+                merge_f.write(str(sort_file[1]))
+                merge_f.write('\n')
+                merge_f.write(data)
+                merge_f.write('\n')
+
+
 pprint(Cookbook().entry_dict())
 pprint(Cookbook().get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
+Merging().merging_files('sorted/1.txt', 'sorted/2.txt', 'sorted/3.txt')
